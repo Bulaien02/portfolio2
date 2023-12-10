3b78430b9da79413e0ca28f2f88b80d990bce865
Bulaien02 <153265908+Bulaien02@users.noreply.github.com>
2023-12-10 23:44:31 +0800

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,10 +4,11 @@ const Footer = () => {
     return(
         <footer className="w-full border-t border-[#33353F]">
             <div className="mb-14 w-full container mx-auto p-4 flex justify-between items-center text-slate-400">
-                <span className="">Yee Chian</span>
+                <span className="">Zi Qing</span>
                 <p>Copyright &copy; All rights reserved</p>
             </div>
         </footer>
     );
 };
 export default Footer;
+
