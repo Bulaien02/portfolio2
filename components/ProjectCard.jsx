b24be35cb56b118d6a6045747756cfce50c60d93
Bulaien02 <153265908+Bulaien02@users.noreply.github.com>
2023-12-10 23:46:13 +0800

    Update ProjectCard.jsx

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,34 +1,68 @@
 import React from "react";
-import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
-//write project card first then only go Projects write the data inside []
-// put parameters inside the function
-const ProjectCard = ({imgUrl, title, description, gitUrl}) => {
-    return(
-        <div>
-            {/* <Image src={imgUrl} height={208} width={1000} className="md:h-72 hover:opacity-30 object-contain bg-no-repeat bg-center rounded-t-xl"/> */}
-            <div
-            className="h-52 rounded-t-xl relative group center center bg-no-repeat"
-            style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
+import ProjectCard from "./ProjectCard";
 
-                <div className="overlay items-center justify-center top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-                    <Link
-                        href={gitUrl}
-                        className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-                    >
-                        <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-                    </Link>
-                </div>
-            </div>
-            
-            <div className="text-white">
-                <h5 className="font-semibold text-xl mb-2">{title}</h5>
-                <p className="text-">{description}</p>
-            </div>
 
+const projectData = [
+    {
+        id: 1,
+        title: "LinkedOut & About",
+        description: "This is a project.",
+        image: "/projects/linkedout&about.png",
+        gitUrl: "https://github.com/APU-Blockchain-Cryptocurrency-Club/LinkedOutAndAbout"
+
+    },
+    {
+        id: 2,
+        title: "ClubDAO",
+        description: "This is a project.",
+        image: "/projects/clubdao.png",
+        gitUrl: "https://github.com/0xYudhishthra/ClubDAO"
+    },
+    {
+        id: 3,
+        title: "FundChat",
+        description: "This is a project for APUBCC Sparkathon & Solana Hyperdrive.",
+        image: "/projects/FundChat.png",
+        gitUrl: "https://github.com/cheongyeechian/fundchat"
+    },
+    {
+        id: 4,
+        title: "Megamask",
+        description: "This is a project for ETH Online.",
+        image: "/projects/megamask.png",
+        gitUrl: "https://github.com/megamask"
+    }, {
+        id: 5,
+        title: "zkJom",
+        description: "This is a project fro ETHKL.",
+        image: "/projects/zkJom.png",
+        gitUrl: "https://github.com/trlau/zkJ0M"
+    }, {
+        id: 6,
+        title: "Attest Me",
+        description: "This is a project for ETH Global Superhack.",
+        image: "/projects/attestme.png",
+        gitUrl: "https://github.com/0xBenjamintan/attestMe"
+    }, 
+
+];
+
+const Projects = () => {
+    return (
+        <div id="projects">
+            <div className="text-4xl font-semibold mb-8">My Projects</div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-rows-2 gap-x-8 gap-y-4 md:gap-12 h-full">
+                {projectData.map((project) => (
+                    <ProjectCard 
+                        key={project.id}
+                        title={project.title}
+                        description={project.description}
+                        imgUrl={project.image}
+                        gitUrl={project.gitUrl}
+                    />))}
+            </div>
         </div>
-    );    
+    );
 };
 
-export default ProjectCard;
-
+export default Projects;
