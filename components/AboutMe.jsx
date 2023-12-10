27f3b345ce89a9026914d3d75bcf47d85da86448
Bulaien02 <153265908+Bulaien02@users.noreply.github.com>
2023-12-10 23:43:10 +0800

    Update AboutMe.jsx

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -9,14 +9,12 @@ const TAB_DATA = [
         id: "skills",
         content: (
           <ul className="list-disc pl-2">
-                <li>React</li>
-                <li>Next.js</li>
-                <li>JavaScript</li>
-                <li>Solidity</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Tailwind CSS</li>
                 <li>Python</li>
+                <li>Java</li>
+                <li>SQL</li>
+                <li>R</li>
+                <li>C</li>
+                <li>System Analysis and Design</li>
           </ul>
         ),
     },
@@ -45,7 +43,7 @@ const TAB_DATA = [
         content: (
           <ul className="list-disc pl-2">
                 <li>Asia Pacific University</li>
-                <li>Tsun Jin High School</li>
+                <li>Chung Ling High School</li>
           </ul>
         ),
     }
@@ -69,9 +67,7 @@ const AboutMe = () => {
                 </div>
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                     <h1 className="text-4xl font-bold text-white mb-4">About Me</h1>
-                    <p className="text-base text-justify lg:text-lg">I am currently pursuing a degree in Cyber Security at APU, where I have been honing my skills in various technologies. I am familiar in frontend development, with expertise in React, Next.js, HTML, CSS, and I'm skilled in creating visually appealing and user-friendly interfaces using Tailwind CSS.
-                        Additionally, I've ventured into the world of blockchain development, particularly with Solidity, allowing me to build smart contracts for decentralized applications.
-                        My passion for technology and problem-solving has led me to actively participate in 11 hackathons this year, where I primarily serve as a Front End Developer. However, I am not limited to frontend tasks and often take on backend responsibilities, demonstrating my versatility and commitment to delivering complete solutions. </p>
+                    <p className="text-base text-justify lg:text-lg">I have attended Asia Pacific University of Technology and Innovation (APU) to further my degree education. I've been specializing in data analytics while studying computer science. My prior coursework consisted of SPM, STPM, and ultimately an APU degree.  </p>
                     <div className="flex flex-row mt-8">
 {/* 1e */}
                         <TabButton 
